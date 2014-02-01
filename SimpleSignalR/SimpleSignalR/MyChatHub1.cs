cb5bef0ea2bdee631988b974af26455bd0e62cdf
SteveHentschel <slhcoding@gmail.com>
2014-02-01 11:12:17 -0500

    SignalR demo from asp.net by Patrick Fletcher "Tutorial: Getting Started with SignalR 2.0"  using VS2013. Simple chat amongst multiple users.  slh

---
+++ SimpleSignalR/SimpleSignalR/MyChatHub1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SimpleSignalR
+{
+    [HubName("MyChatHub1")]                 // ensure no camel case error in $.connection script (on html page)
+    public class MyChatHub1 : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
